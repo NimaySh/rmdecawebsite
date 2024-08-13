eb94b3ca523ce13a8157dd7e90faa1208f34ca10
Parim Shah <shahparim3@gmail.com>
2024-08-13 14:29:27 -0400

    updated navigation.js and startup.astro

--- src/navigation.js
+++ src/navigation.js
@@ -115,7 +115,6 @@ export const headerData = {
       href: '#',
     },
   ],
-  actions: [{ text: 'Download', href: 'https://github.com/onwidget/astrowind', target: '_blank' }],
 };
 
 export const footerData = {
