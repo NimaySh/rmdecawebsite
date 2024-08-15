61931dab1a6a5b3bb1a8f3b210fd4c2aed6f2256
Parim Shah <shahparim3@gmail.com>
2024-08-14 22:37:09 -0400

    updating nav and footer

--- src/navigation.js
+++ src/navigation.js
@@ -114,68 +114,54 @@ export const headerData = {
 //      ],
 //    },
   ],
+ actions: [{ text: 'Join DECA', 
+              href: ' ', 
+              target: '_blank' }],
 };
 
 export const footerData = {
   links: [
     {
-      title: 'Product',
+      title: 'About',
       links: [
-        { text: 'Features', href: '#' },
-        { text: 'Security', href: '#' },
-        { text: 'Team', href: '#' },
-        { text: 'Enterprise', href: '#' },
-        { text: 'Customer stories', href: '#' },
-        { text: 'Pricing', href: '#' },
+        { text: 'What we do', href: '#' },
+        { text: 'Our Team', href: '#' },
+        { text: 'FAQ', href: '#' },
+      ],
+    },
+    {
+      title: 'Competitions',
+      links: [
+        { text: 'Regionals', href: '#' },
+        { text: 'States', href: '#' },
+        { text: 'Internationals', href: '#' },
+      ],
+    },
+    {
+      title: 'Learn',
+      links: [
         { text: 'Resources', href: '#' },
+        { text: 'DECA 101', href: '#' },
       ],
     },
     {
-      title: 'Platform',
+      title: 'Other',
       links: [
-        { text: 'Developer API', href: '#' },
-        { text: 'Partners', href: '#' },
-        { text: 'Atom', href: '#' },
-        { text: 'Electron', href: '#' },
-        { text: 'AstroWind Desktop', href: '#' },
-      ],
-    },
-    {
-      title: 'Support',
-      links: [
-        { text: 'Docs', href: '#' },
-        { text: 'Community Forum', href: '#' },
-        { text: 'Professional Services', href: '#' },
-        { text: 'Skills', href: '#' },
-        { text: 'Status', href: '#' },
-      ],
-    },
-    {
-      title: 'Company',
-      links: [
-        { text: 'About', href: '#' },
-        { text: 'Blog', href: '#' },
-        { text: 'Careers', href: '#' },
-        { text: 'Press', href: '#' },
-        { text: 'Inclusion', href: '#' },
-        { text: 'Social Impact', href: '#' },
-        { text: 'Shop', href: '#' },
+        { text: 'Maryland DECA', href: '#' },
+        { text: 'National DECA', href: '#' },
       ],
     },
   ],
   secondaryLinks: [
-    { text: 'Terms', href: getPermalink('/terms') },
-    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+    { text: 'Terms', href: getPermalink('/') },
+    { text: 'Privacy Policy', href: getPermalink('/') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
-    { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
-    { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/onwidget/astrowind' },
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/rmhsdeca/' },
+    { ariaLabel: 'Discord', icon: 'tabler:brand-discord', href: 'https://discord.gg/Gp3YmpDzQd' },
+    { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/rmhs_deca' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="<insert RM Deca Logo>" alt="onWidget logo" loading="lazy"></img>
-    Made by RM DECA · All rights reserved.
-  `,
+    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="src/assets/favicons/apple-touch-icon.png" alt="onWidget logo" loading="lazy"></img>
+    Created and maintained by Parim and Nimay of RM DECA`,
 };
