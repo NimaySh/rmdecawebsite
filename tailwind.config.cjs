cc2f4fabe2c017bf37983a40a28ff5d31d79e65f
Parim Shah <shahparim3@gmail.com>
2024-12-21 16:17:55 -0500

    fixing styles again

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,26 +2,33 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 import typographyPlugin from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
-  darkMode: 'media', // Explicitly disable dark mode
   theme: {
     extend: {
       colors: {
         primary: '#2979a2',
         secondary: '#35a5c2',
         accent: 'var(--aw-color-accent)',
-      },
-      textColor: {
         default: 'var(--aw-color-text-default)',
-        muted: '#718096',
+        muted: '#6b7280',
       },
       fontFamily: {
         sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
         serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
         heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
       },
+
+      animation: {
+        fade: 'fadeInUp 1s both',
+      },
+
+      keyframes: {
+        fadeInUp: {
+          '0%': { opacity: 0, transform: 'translateY(2rem)' },
+          '100%': { opacity: 1, transform: 'translateY(0)' },
+        },
+      },
     },
   },
-  plugins: [typographyPlugin],
 };
